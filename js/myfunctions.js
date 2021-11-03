6e0e032408024df0c135e98ce6edc7edf99d9899
Eliachar Feig <efeig15@gmail.com>
2021-11-03 14:34:21 +0200

    2

--- js/myfunctions.js
+++ js/myfunctions.js
@@ -1,1 +1,31 @@
-/* your JS code here */
+
+/*document.getElementById("myBtn").onclick = function() {addButton()};*/
+
+function addButton() {
+    let input = document.getElementById("titleID");
+    alert(input.value)
+
+    let y = input.elements["name"].value;
+    document.getElementById("demo").innerHTML += input.value;/*"Hi" + y + "<br>"*/
+}
+
+/*
+    $(document).ready(function(){
+        $('[data-toggle="popover"]').popover();
+    });
+*/
+
+
+
+/*
+
+    /!*  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+})*!/
+*/
